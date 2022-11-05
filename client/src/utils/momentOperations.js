0c4213d177c040eeacce49922789afbfe3fe31e1
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2022-11-05 19:08:26 -0400

    add momentOperations

---
+++ client/src/utils/momentOperations.js
@@ -0,0 +1,19 @@
+import moment from "moment";
+
+export const splitEventsByDay = (events) => {
+  let result = [[], [], [], [], [], [], []];
+  events.forEach((e) => {
+    result[e.day()].push(e);
+  });
+  return result;
+};
+
+export const incrementBy = (date, hours) => {
+  return date.clone().add(hours, "hours");
+};
+
+export const transposeToThisWeek = (date) => {
+  const offset = date - date?.clone()?.startOf("week");
+  const newDate = moment(moment() + offset);
+  return newDate;
+};
