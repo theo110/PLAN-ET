fedc46fde0a7c34b32c6dc518ef140cbab98b065
Theo Roh <shouyuanroh@gmail.com>
2022-11-05 14:11:20 -0400

    finished form

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,6 +3,14 @@ import { Typography, Button, TextField, InputAdornment } from '@mui/material';
 import { useFormik } from 'formik';
 
 const Form = (props) => {
+    const [customField, setCustomField] = useState([]);
+    const [custom, setCustom] = useState("");
+
+    const addEntry = () => {
+        setCustomField([...customField, custom])
+        console.log(customField)
+    }
+
     const formik = useFormik({
         initialValues: {
             sleep: '',
@@ -11,57 +19,114 @@ const Form = (props) => {
         onSubmit: props.submitHandler,
     });
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="sleep">How long do you sleep?
-                <input
-                    id="sleep"
-                    name="sleep"
-                    type="number"
-                    min="0"
-                    onChange={formik.handleChange}
-                    value={formik.values.sleep}
-                />
-                <span>hrs/day</span>
-            </label>
-            <label htmlFor="study">How long do you study?
-                <input
-                    id="study"
-                    name="study"
-                    type="number"
-                    min="0"
-                    onChange={formik.handleChange}
-                    value={formik.values.study}
-                />
-                <span>hrs/day</span>
-            </label>
-            <button type="submit">Submit</button>
-        </form>
+        <div>
+            <form onSubmit={formik.handleSubmit} id="form">
+                <label htmlFor="sleep">How long do you sleep?
+                    <input
+                        id="sleep"
+                        name="sleep"
+                        type="number"
+                        min="0"
+                        onChange={formik.handleChange}
+                        value={formik.values.sleep}
+                    />
+                    <span>hrs/day</span>
+                </label>
+                <label htmlFor="study">How long do you study?
+                    <input
+                        id="study"
+                        name="study"
+                        type="number"
+                        min="0"
+                        onChange={formik.handleChange}
+                        value={formik.values.study}
+                    />
+                    <span>hrs/day</span>
+                </label>
+                <label htmlFor="meal">How long do typically spend eating?
+                    <input
+                        id="meal"
+                        name="meal"
+                        type="number"
+                        min="0"
+                        onChange={formik.handleChange}
+                        value={formik.values.meal}
+                    />
+                    <span>hrs/meal</span>
+                </label>
+                {customField.map((field) => (
+                    <label htmlFor="custom">How long do typically spend {field}?
+                        <input
+                            id={field}
+                            name={field}
+                            type="number"
+                            min="0"
+                            onChange={formik.handleChange}
+                            value={formik.values[field]}
+                        />
+                        <span>hrs/day</span>
+                    </label>
+                ))}
+
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                <label htmlFor="custom">Add your own custom event:
+                    <input
+                        id="custom"
+                        name="custom"
+                        type="string"
+                        onChange={e => { setCustom(e.target.value) }}
+                    />
+                </label>
+                <button onClick={addEntry}>Add field</button>
+            </div>
+        </div>
     )
 }
 function Home(props) {
     const [fixedEvents, setFixedEvents] = useState([]);
     const [otherEvents, setOtherEvents] = useState([]);
 
-    useEffect(()=>{
-        if(otherEvents.length!==0){
-            alert(JSON.stringify(otherEvents, null, 2));
+    useEffect(() => {
+        if (otherEvents.length !== 0) {
+            console.log(JSON.stringify(otherEvents, null, 2));
         }
-    },[otherEvents])
-    
+    }, [otherEvents])
+
     //Aggregate form data
     const onSubmit = (values) => {
         const keys = Object.keys(values)
         var i = 0
         var array = []
-        while(i < keys.length){
+
+        var currPriority = 3;
+        while (i < keys.length) {
+            var priority;
             var key = keys[i]
-            var curr = { name: key, time: values[key] }
+            switch(key){
+                case 'sleep':
+                    priority=0;
+                    break;
+                case 'meal':
+                    priority=1;
+                    break;
+                case 'study':
+                    priority=2;
+                    break;
+                default:
+                    priority=currPriority;
+                    currPriority += 1;
+            }
+            var curr = { name: key, time: values[key], priority: priority}
             array.push(curr);
             i++;
         }
         setOtherEvents(array)
     }
 
+
+    //Uploader
     if (fixedEvents.length === 0) {
         return (
             <div>
