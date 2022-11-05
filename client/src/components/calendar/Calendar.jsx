bb9746d8f50050120277cc2bacbf6d74c8d37391
Thomas Hart <thomas.hart@uwaterloo.ca>
2022-11-05 12:31:35 -0400

    file input

--- client/src/components/calendar/Calendar.jsx
+++ client/src/components/calendar/Calendar.jsx
@@ -3,15 +3,23 @@ import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
 
 const Calendar = () => {
+  const handleFile = async (e) => {
+    const file = e.target.files[0];
+    console.log(file);
+  }
+
   return (
-    <FullCalendar
-      plugins={[timeGridPlugin]}
-      initialView='timeGridWeek'
-      events={[
-        { title: "event 1", start: "2022-11-05T03:24:00", end: "2022-11-05T05:24:00" },
-        { title: "event 1", start: "2022-11-05T07:24:00", end: "2022-11-05T12:24:00" },
-      ]}
-    />
+    <div>
+      <input onChange={handleFile} type='file' />
+      <FullCalendar
+        plugins={[timeGridPlugin]}
+        initialView='timeGridWeek'
+        events={[
+          { title: "event 1", start: "2022-11-05T03:24:00", end: "2022-11-05T05:24:00" },
+          { title: "event 1", start: "2022-11-05T07:24:00", end: "2022-11-05T12:24:00" },
+        ]}
+      />
+    </div>
   );
 };
 
