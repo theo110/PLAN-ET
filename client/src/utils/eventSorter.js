8bf9487bd96cb8f13a4fd0258eb6f57d670c72a3
Theo Roh <shouyuanroh@gmail.com>
2022-11-05 22:17:19 -0400

    fix

--- client/src/utils/eventSorter.js
+++ client/src/utils/eventSorter.js
@@ -23,11 +23,11 @@ export const getPotential = (events, start, end) => {
   return potential;
 }
 
-const algorithm = (fixedEvents, freeEvents, start) => {
+export const algorithm = (fixedEvents, freeEvents, start) => {
   var allEvents = [];
   var allPotentials = []
   for (var i = 0; i < 7; i++) {
-    allPotentials[i].append(getPotential(fixedEvents, incrementBy(start,24*i), incrementBy(start, 24*(i+1))));
+    allPotentials[i].push(getPotential(fixedEvents, incrementBy(start,24*i), incrementBy(start, 24*(i+1))));
     console.log(allPotentials)
   }
 
@@ -38,7 +38,7 @@ const algorithm = (fixedEvents, freeEvents, start) => {
     //12 - x not enough
     if (hourDifferenceBetweenDates(allPotentials[i][0][1],allPotentials[i][0][0])< fixedEvents[0].time) {
       //Subtract time from current day
-      allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][0][0], end: allPotentials[i][0][1] })
+      allEvents[i].push({ title: freeEvents[k].name, start: allPotentials[i][0][0], end: allPotentials[i][0][1] })
       fixedEvents[0].time -= hourDifferenceBetweenDates(allPotentials[i][0][1] - allPotentials[i][0][0])
       allPotentials[i].remove()
 
@@ -48,12 +48,12 @@ const algorithm = (fixedEvents, freeEvents, start) => {
         return [];
       } else {
         //Subtract time from night
-        allEvents[i].append({ title: freeEvents[k].name, start: incrementBy(allPotentials[compliment][allPotentials[compliment].length - 1][1],-fixedEvents[0].time), end: allPotentials[compliment][allPotentials[compliment].length - 1][1] })
+        allEvents[i].push({ title: freeEvents[k].name, start: incrementBy(allPotentials[compliment][allPotentials[compliment].length - 1][1],-fixedEvents[0].time), end: allPotentials[compliment][allPotentials[compliment].length - 1][1] })
         allPotentials[compliment][allPotentials[compliment].length - 1][1] = incrementBy(allPotentials[compliment][allPotentials[compliment].length - 1][1],fixedEvents[0].time);
       }
     } else {
       //Can sleep 12 - x 
-      allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][0][0], end: incrementBy(allPotentials[i][0][0],fixedEvents[0].time)})
+      allEvents[i].push({ title: freeEvents[k].name, start: allPotentials[i][0][0], end: incrementBy(allPotentials[i][0][0],fixedEvents[0].time)})
       allPotentials[i][0][0] = incrementBy(allPotentials[i][0][0],fixedEvents[0].time);
     }
 
@@ -111,14 +111,14 @@ const algorithm = (fixedEvents, freeEvents, start) => {
         if (hourDifferenceBetweenDates(allPotentials[i][l][1],allPotentials[i][l][0])< freeEvents[k].time) {
 
           //Make sure end - start = allPotenials[i][l][1] - allPotentials[i][l][0]
-          allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][l][0], end: allPotentials[i][l][1]})
+          allEvents[i].push({ title: freeEvents[k].name, start: allPotentials[i][l][0], end: allPotentials[i][l][1]})
           freeEvents[k].time -= hourDifferenceBetweenDates(allPotentials[i][l][1],allPotentials[i][l][0]);
           allPotentials[i].remove(l);
         }
         //Period > time
         else {
           //Make sure end - start = freeEvents[k].time
-          allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][l][0], end: incrementBy(allPotentials[i][l][0],freeEvents[k].time)})
+          allEvents[i].push({ title: freeEvents[k].name, start: allPotentials[i][l][0], end: incrementBy(allPotentials[i][l][0],freeEvents[k].time)})
           allPotentials[i][l][0] = incrementBy(allPotentials[i][l][0],freeEvents[k].time)
           freeEvents[k].time = 0;
           break;
