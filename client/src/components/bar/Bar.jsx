200d1bfa553c3105ccf5ec55116f9be7eaf29d68
Thomas Hart <thomas.hart@uwaterloo.ca>
2022-11-06 03:46:46 -0500

    Added logo to nav bar

--- client/src/components/bar/Bar.jsx
+++ client/src/components/bar/Bar.jsx
@@ -1,13 +1,15 @@
 import AuthenticationButton from "../auth/authentication-button";
 import "./bar.css";
 import { AppBar, Typography, Toolbar } from "@mui/material";
+import { ReactComponent as Logo } from "../../assets/logo.svg"
 
 const Bar = () => {
   return (
     <div>
       <AppBar position="absolute" color="tertiary">
         <Toolbar>
-          <Typography>Plan-et</Typography>
+          <Logo className="logo" width="3rem" />
+          <Typography style={{ fontWeight: 600, fontSize: "2rem" }}>PLAN-ET</Typography>
           <AuthenticationButton />
         </Toolbar>
       </AppBar>
