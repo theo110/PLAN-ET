399b836a08ad2ab1225e6644a49ef7c689a1b2ae
Ayush Ganguly <ayushganguly@Ayushs-MacBook-Pro-2.local>
2022-11-05 22:43:34 -0400

    only logged in users can see calendar

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,165 +1,179 @@
 import { useState, useEffect } from "react";
-import { Typography, Button, TextField, InputAdornment } from "@mui/material";
 import { useFormik } from "formik";
 import { useNavigate } from "react-router";
 import Upload from "../upload/Upload";
 import "./Home.css";
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Form = (props) => {
-    const [customField, setCustomField] = useState([]);
-    const [custom, setCustom] = useState("");
+  const [customField, setCustomField] = useState([]);
+  const [custom, setCustom] = useState("");
 
-    const addEntry = () => {
-        setCustomField([...customField, custom])
-        console.log(customField)
-    }
+  const addEntry = () => {
+    setCustomField([...customField, custom]);
+    console.log(customField);
+  };
 
-    const formik = useFormik({
-        initialValues: {
-            sleep: '',
-            study: '',
-        },
-        onSubmit: props.submitHandler,
-    });
-    return (
-        <div>
-            <form onSubmit={formik.handleSubmit} id="form">
-                <label htmlFor="sleep">How long do you sleep?
-                    <input
-                        id="sleep"
-                        name="sleep"
-                        type="number"
-                        min="0"
-                        onChange={formik.handleChange}
-                        value={formik.values.sleep}
-                    />
-                    <span>hrs/day</span>
-                </label>
-                <label htmlFor="study">How long do you study?
-                    <input
-                        id="study"
-                        name="study"
-                        type="number"
-                        min="0"
-                        onChange={formik.handleChange}
-                        value={formik.values.study}
-                    />
-                    <span>hrs/day</span>
-                </label>
-                <label htmlFor="meal">How long do typically spend eating?
-                    <input
-                        id="meal"
-                        name="meal"
-                        type="number"
-                        min="0"
-                        onChange={formik.handleChange}
-                        value={formik.values.meal}
-                    />
-                    <span>hrs/meal</span>
-                </label>
-                {customField.map((field) => (
-                    <label htmlFor="custom">How long do typically spend {field}?
-                        <input
-                            id={field}
-                            name={field}
-                            type="number"
-                            min="0"
-                            onChange={formik.handleChange}
-                            value={formik.values[field]}
-                        />
-                        <span>hrs/day</span>
-                    </label>
-                ))}
-
-                <button type="submit">Submit</button>
-            </form>
-            <div>
-                <label htmlFor="custom">Add your own custom event:
-                    <input
-                        id="custom"
-                        name="custom"
-                        type="string"
-                        onChange={e => { setCustom(e.target.value) }}
-                    />
-                </label>
-                <button onClick={addEntry}>Add field</button>
-            </div>
-        </div>
-    )
-}
-
-function Home(props) {
-    const { eventEntries, setEventEntries } = props;
-    const [fixedEvents, setFixedEvents] = useState([]);
-    const [otherEvents, setOtherEvents] = useState([]);
-
-
-    function sortByPriority(otherEvents){
-        const result = [...otherEvents].sort(function(a, b) {
-          if (a.priority < b.priority) return -1;
-          if (a.priority > b.priority) return 1;
-          return 0
-        })
-        return result
-    }
-
-    useEffect(() => {        
-        if (otherEvents.length !== 0) {
-            console.log(JSON.stringify(otherEvents));
-        }
-    }, [otherEvents])
-
-    const testCal = () => {
-        setEventEntries(fixedEvents);
-        console.log(fixedEvents)
-        navigate("/calendar")
-    }
-
-
-    //Aggregate form data
-    const onSubmit = (values) => {
-        const keys = Object.keys(values)
-        var i = 0
-        var array = []
-
-        var currPriority = 3;
-        while (i < keys.length) {
-            var priority;
-            var key = keys[i]
-            switch (key) {
-                case 'sleep':
-                    priority = 0;
-                    break;
-                case 'meal':
-                    priority = 1;
-                    break;
-                case 'study':
-                    priority = 2;
-                    break;
-                default:
-                    priority = currPriority;
-                    currPriority += 1;
-            }
-            var curr = { name: key, time: values[key], priority: priority }
-            array.push(curr);
-            i++;
-        }
-        var sorted = sortByPriority(array)
-        setOtherEvents([sorted])
-    }
-
-    // test calendar
-    const navigate = useNavigate();
-
+  const formik = useFormik({
+    initialValues: {
+      sleep: "",
+      study: "",
+    },
+    onSubmit: props.submitHandler,
+  });
   return (
-    <div className="container">
-      <button type='button' onClick={() => testCal()}>
-        Test calendar
-      </button>
-      {(Object.keys(fixedEvents).length === 0) ? <Upload setFixedEvents={setFixedEvents} /> :  <Form submitHandler={onSubmit}></Form>}
+    <div>
+      <form onSubmit={formik.handleSubmit} id="form">
+        <label htmlFor="sleep">
+          How long do you sleep?
+          <input
+            id="sleep"
+            name="sleep"
+            type="number"
+            min="0"
+            onChange={formik.handleChange}
+            value={formik.values.sleep}
+          />
+          <span>hrs/day</span>
+        </label>
+        <label htmlFor="study">
+          How long do you study?
+          <input
+            id="study"
+            name="study"
+            type="number"
+            min="0"
+            onChange={formik.handleChange}
+            value={formik.values.study}
+          />
+          <span>hrs/day</span>
+        </label>
+        <label htmlFor="meal">
+          How long do typically spend eating?
+          <input
+            id="meal"
+            name="meal"
+            type="number"
+            min="0"
+            onChange={formik.handleChange}
+            value={formik.values.meal}
+          />
+          <span>hrs/meal</span>
+        </label>
+        {customField.map((field) => (
+          <label htmlFor="custom">
+            How long do typically spend {field}?
+            <input
+              id={field}
+              name={field}
+              type="number"
+              min="0"
+              onChange={formik.handleChange}
+              value={formik.values[field]}
+            />
+            <span>hrs/day</span>
+          </label>
+        ))}
+
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <label htmlFor="custom">
+          Add your own custom event:
+          <input
+            id="custom"
+            name="custom"
+            type="string"
+            onChange={(e) => {
+              setCustom(e.target.value);
+            }}
+          />
+        </label>
+        <button onClick={addEntry}>Add field</button>
+      </div>
     </div>
   );
+};
+
+function Home(props) {
+  const { eventEntries, setEventEntries } = props;
+  const [fixedEvents, setFixedEvents] = useState([]);
+  const [otherEvents, setOtherEvents] = useState([]);
+
+  function sortByPriority(otherEvents) {
+    const result = [...otherEvents].sort(function (a, b) {
+      if (a.priority < b.priority) return -1;
+      if (a.priority > b.priority) return 1;
+      return 0;
+    });
+    return result;
+  }
+
+  useEffect(() => {
+    if (otherEvents.length !== 0) {
+      console.log(JSON.stringify(otherEvents));
+    }
+  }, [otherEvents]);
+
+  const testCal = () => {
+    setEventEntries(fixedEvents);
+    console.log(fixedEvents);
+    navigate("/calendar");
+  };
+
+  //Aggregate form data
+  const onSubmit = (values) => {
+    const keys = Object.keys(values);
+    var i = 0;
+    var array = [];
+
+    var currPriority = 3;
+    while (i < keys.length) {
+      var priority;
+      var key = keys[i];
+      switch (key) {
+        case "sleep":
+          priority = 0;
+          break;
+        case "meal":
+          priority = 1;
+          break;
+        case "study":
+          priority = 2;
+          break;
+        default:
+          priority = currPriority;
+          currPriority += 1;
+      }
+      var curr = { name: key, time: values[key], priority: priority };
+      array.push(curr);
+      i++;
+    }
+    var sorted = sortByPriority(array);
+    setOtherEvents([sorted]);
+  };
+
+  // test calendar
+  const navigate = useNavigate();
+
+  const { isAuthenticated } = useAuth0();
+  const seeCalendar =
+    isAuthenticated === true ? (
+      <div>
+        <button type="button" onClick={() => testCal()}>
+          Test calendar
+        </button>
+        {Object.keys(fixedEvents).length === 0 ? (
+          <Upload setFixedEvents={setFixedEvents} />
+        ) : (
+          <Form submitHandler={onSubmit}></Form>
+        )}
+      </div>
+    ) : (
+      <p>Please log in to use</p>
+    );
+
+  return <div className="container">{seeCalendar}</div>;
 }
 
 export default Home;
--- client/src/utils/eventSorter.js
+++ client/src/utils/eventSorter.js
@@ -1,4 +1,4 @@
-import { incrementBy, hourDifferenceBetweenDates} from "./momentOperations";
+import { incrementBy, hourDifferenceBetweenDates } from "./momentOperations";
 
 const sortEvents = (eventEntries) => {
   const chronologicalComparator = (e1, e2) => {
@@ -23,49 +23,102 @@ const getPotential = (events, start, end) => {
     }
   }
   return potential;
-}
+};
 
 const algorithm = (fixedEvents, freeEvents, start) => {
   var allEvents = [];
-  var allPotentials = []
+  var allPotentials = [];
   for (var i = 0; i < 7; i++) {
-    allPotentials[i].append(getPotential(fixedEvents, incrementBy(start,24*i), incrementBy(start, 24*(i+1))));
-    console.log(allPotentials)
+    allPotentials[i].append(
+      getPotential(
+        fixedEvents,
+        incrementBy(start, 24 * i),
+        incrementBy(start, 24 * (i + 1))
+      )
+    );
+    console.log(allPotentials);
   }
 
   //Iterate through each day
   for (var i = 0; i < 7; i++) {
     //Sleep Time
-    var compliment = (i == 0) ? 6 : i - 1;
+    var compliment = i == 0 ? 6 : i - 1;
     //12 - x not enough
-    if (hourDifferenceBetweenDates(allPotentials[i][0][1],allPotentials[i][0][0])< fixedEvents[0].time) {
+    if (
+      hourDifferenceBetweenDates(
+        allPotentials[i][0][1],
+        allPotentials[i][0][0]
+      ) < fixedEvents[0].time
+    ) {
       //Subtract time from current day
-      allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][0][0], end: allPotentials[i][0][1] })
-      fixedEvents[0].time -= hourDifferenceBetweenDates(allPotentials[i][0][1] - allPotentials[i][0][0])
-      allPotentials[i].remove()
+      allEvents[i].append({
+        title: freeEvents[k].name,
+        start: allPotentials[i][0][0],
+        end: allPotentials[i][0][1],
+      });
+      fixedEvents[0].time -= hourDifferenceBetweenDates(
+        allPotentials[i][0][1] - allPotentials[i][0][0]
+      );
+      allPotentials[i].remove();
 
       //Check time at previous night
-      if (hourDifferenceBetweenDates(allPotentials[compliment][allPotentials[compliment].length - 1][1],allPotentials[compliment][allPotentials[compliment].length - 1][0]) < fixedEvents[0].time) {
+      if (
+        hourDifferenceBetweenDates(
+          allPotentials[compliment][allPotentials[compliment].length - 1][1],
+          allPotentials[compliment][allPotentials[compliment].length - 1][0]
+        ) < fixedEvents[0].time
+      ) {
         //If not possible, then can't sleep
         return [];
       } else {
         //Subtract time from night
-        allEvents[i].append({ title: freeEvents[k].name, start: incrementBy(allPotentials[compliment][allPotentials[compliment].length - 1][1],-fixedEvents[0].time), end: allPotentials[compliment][allPotentials[compliment].length - 1][1] })
-        allPotentials[compliment][allPotentials[compliment].length - 1][1] = incrementBy(allPotentials[compliment][allPotentials[compliment].length - 1][1],fixedEvents[0].time);
+        allEvents[i].append({
+          title: freeEvents[k].name,
+          start: incrementBy(
+            allPotentials[compliment][allPotentials[compliment].length - 1][1],
+            -fixedEvents[0].time
+          ),
+          end: allPotentials[compliment][
+            allPotentials[compliment].length - 1
+          ][1],
+        });
+        allPotentials[compliment][allPotentials[compliment].length - 1][1] =
+          incrementBy(
+            allPotentials[compliment][allPotentials[compliment].length - 1][1],
+            fixedEvents[0].time
+          );
       }
     } else {
-      //Can sleep 12 - x 
-      allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][0][0], end: incrementBy(allPotentials[i][0][0],fixedEvents[0].time)})
-      allPotentials[i][0][0] + incrementBy(allPotentials[i][0][0],fixedEvents[0].time);
+      //Can sleep 12 - x
+      allEvents[i].append({
+        title: freeEvents[k].name,
+        start: allPotentials[i][0][0],
+        end: incrementBy(allPotentials[i][0][0], fixedEvents[0].time),
+      });
+      // allPotentials[i][0][0] + incrementBy(allPotentials[i][0][0],fixedEvents[0].time);
     }
 
     //Meal Time
     //Breakfast
     let j = 0;
     let found = false;
-    for (; j < allPotentials[i].length && incrementBy(allPotentials[i][j][0], freeEvents[1].time).hour <= 10; ++j) {
-      if (hourDifferenceBetweenDates(allPotentials[i][j][1], allPotentials[i][j][0]) >= freeEvents[1].time) {
-        allEvents[i].push({title: freeEvents[1].name, start: allPotentials[i][j][0], end: incrementBy(allPotentials[i][j][0], freeEvents[1].time)});
+    for (
+      ;
+      j < allPotentials[i].length &&
+      incrementBy(allPotentials[i][j][0], freeEvents[1].time).hour <= 10;
+      ++j
+    ) {
+      if (
+        hourDifferenceBetweenDates(
+          allPotentials[i][j][1],
+          allPotentials[i][j][0]
+        ) >= freeEvents[1].time
+      ) {
+        allEvents[i].push({
+          title: freeEvents[1].name,
+          start: allPotentials[i][j][0],
+          end: incrementBy(allPotentials[i][j][0], freeEvents[1].time),
+        });
         allPotentials[i][j][0].hours += freeEvents[1].time;
         found = true;
         break;
@@ -76,13 +129,33 @@ const algorithm = (fixedEvents, freeEvents, start) => {
     }
     found = false;
     //Lunch
-    for (; j < allPotentials[i].length && (allPotentials[i][j][0].hour >= 11 || incrementBy(allPotentials[i][j][1], -freeEvents[1].time).hour >= 11) && incrementBy(allPotentials[i][j][0], freeEvents[1].time).hour <= 2; ++j) {
-      if (hourDifferenceBetweenDates(allPotentials[i][j][1], allPotentials[i][j][0]) >= freeEvents[1].time) {
+    for (
+      ;
+      j < allPotentials[i].length &&
+      (allPotentials[i][j][0].hour >= 11 ||
+        incrementBy(allPotentials[i][j][1], -freeEvents[1].time).hour >= 11) &&
+      incrementBy(allPotentials[i][j][0], freeEvents[1].time).hour <= 2;
+      ++j
+    ) {
+      if (
+        hourDifferenceBetweenDates(
+          allPotentials[i][j][1],
+          allPotentials[i][j][0]
+        ) >= freeEvents[1].time
+      ) {
         if (allPotentials[i][j][0].hours >= 11) {
-          allEvents[i].push({title: freeEvents[1].name, start: allPotentials[i][j][0], end: incrementBy(allPotentials[i][j][0], freeEvents[i].time)});
+          allEvents[i].push({
+            title: freeEvents[1].name,
+            start: allPotentials[i][j][0],
+            end: incrementBy(allPotentials[i][j][0], freeEvents[i].time),
+          });
           allPotentials[i][j][0].hours += freeEvents[1].time;
         } else {
-          allEvents[i].push({title: freeEvents[1].name, start: incrementBy(allPotentials[i][j][1], -freeEvents[1].time), end: allPotentials[i][j][1]});
+          allEvents[i].push({
+            title: freeEvents[1].name,
+            start: incrementBy(allPotentials[i][j][1], -freeEvents[1].time),
+            end: allPotentials[i][j][1],
+          });
           allPotentials[i][j][1].hours -= freeEvents[1].time;
         }
         found = true;
@@ -94,9 +167,22 @@ const algorithm = (fixedEvents, freeEvents, start) => {
     }
     found = false;
     //Dinner
-    for (; j < allPotentials[i].length && allPotentials[i][j][0].hours >= 5; ++j) {
-      if (hourDifferenceBetweenDates(allPotentials[i][j][1], allPotentials[i][j][0]) >= freeEvents[1].time) {
-        allEvents[i].push({title: freeEvents[1].name, start: allPotentials[i][j][0], end: incrementBy(allPotentials[i][j][0], freeEvents[i].time)});
+    for (
+      ;
+      j < allPotentials[i].length && allPotentials[i][j][0].hours >= 5;
+      ++j
+    ) {
+      if (
+        hourDifferenceBetweenDates(
+          allPotentials[i][j][1],
+          allPotentials[i][j][0]
+        ) >= freeEvents[1].time
+      ) {
+        allEvents[i].push({
+          title: freeEvents[1].name,
+          start: allPotentials[i][j][0],
+          end: incrementBy(allPotentials[i][j][0], freeEvents[i].time),
+        });
         allPotentials[i][j][0].hours += freeEvents[1].time;
         found = true;
         break;
@@ -110,25 +196,42 @@ const algorithm = (fixedEvents, freeEvents, start) => {
     for (var k = 2; k < freeEvents.length; k++) {
       for (var l = 0; i < allPotentials.length; i++) {
         //Split up event
-        if (hourDifferenceBetweenDates(allPotentials[i][l][1],allPotentials[i][l][0])< freeEvents[k].time) {
-
+        if (
+          hourDifferenceBetweenDates(
+            allPotentials[i][l][1],
+            allPotentials[i][l][0]
+          ) < freeEvents[k].time
+        ) {
           //Make sure end - start = allPotenials[i][l][1] - allPotentials[i][l][0]
-          allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][l][0], end: allPotentials[i][l][1]})
-          freeEvents[k].time -= hourDifferenceBetweenDates(allPotentials[i][l][1],allPotentials[i][l][0]);
+          allEvents[i].append({
+            title: freeEvents[k].name,
+            start: allPotentials[i][l][0],
+            end: allPotentials[i][l][1],
+          });
+          freeEvents[k].time -= hourDifferenceBetweenDates(
+            allPotentials[i][l][1],
+            allPotentials[i][l][0]
+          );
           allPotentials[i].remove(l);
         }
         //Period > time
         else {
           //Make sure end - start = freeEvents[k].time
-          allEvents[i].append({ title: freeEvents[k].name, start: allPotentials[i][l][0], end: incrementBy(allPotentials[i][l][0],freeEvents[k].time)})
-          allPotentials[i][l][0] = incrementBy(allPotentials[i][l][0],freeEvents[k].time)
+          allEvents[i].append({
+            title: freeEvents[k].name,
+            start: allPotentials[i][l][0],
+            end: incrementBy(allPotentials[i][l][0], freeEvents[k].time),
+          });
+          allPotentials[i][l][0] = incrementBy(
+            allPotentials[i][l][0],
+            freeEvents[k].time
+          );
           freeEvents[k].time = 0;
           break;
         }
-
       }
-      if (freeEvents[k].time > 0) return []
+      if (freeEvents[k].time > 0) return [];
     }
   }
   return allEvents;
-}
+};
